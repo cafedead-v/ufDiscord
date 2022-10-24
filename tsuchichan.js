3b5fbffe1cf64ac49aa65afb607c2c0ce7e1d6b8
cafedead-v <>
2022-10-24 16:17:33 +0900

    mod tsuchichan

--- tsuchichan.js
+++ tsuchichan.js
@@ -1,57 +1,55 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const client = new Client({
+constclient = newClient({
 	intents: [
 		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembersGatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildVoiceStates,
-	]
+		GatewayIntentBits.GuildVoiceStates]
 });
 require('dotenv').config();
 const { tsuchichan } = process.env;
 client.login(process.env.tsuchichan);
-
-client.once('ready', () => {
-	console.log(`ready, logged in as ${client.user.username}!`);
+client.once('ready', () => { console.log(`ready,loggedinas${client.user.username}!`);
 });
 
 //member add
 client.on('guildMemberAdd', member => {
 	member.user.displayAvatarURL
-	const welcomeEmbed = new EmbedBuilder()
-		.setColor(0x9999FF)
-		.setDescription(`${member.guild.name}に${member.user.username}が参加しました`)
+	constwelcomeEmbed = newEmbedBuilder()
+		.setColor(0x99FFFF)
+		.setDescription(`${member.guild.name}に${member.user.username}が参加したよ`)
 		.setThumbnail(displayAvatarURL)
 	member.guild.channels.cache.get('992123731277258935').send({ 'embeds': [welcomeEmbed] });
-	console.log(`${member.guild.name}に${member.user.username}が参加しました`);
+	console.log(`${member.guild.name}に${member.user.username}が参加したよ`);
 });
 
 //member removed
 client.on('guildMemberRemove', member => {
-	member.user.displayAvatarURL
-	const goodbyeEmbed = new EmbedBuilder()
-		.setColor(0x9999FF)
-		.setDescription(`${member.guild.name}から${member.user.username}が退出しました`)
+	member.user.displayAvatarURLconstgoodbyeEmbed = newEmbedBuilder()
+		.setColor(0x99FFFF)
+		.setDescription(`${member.guild.name}から${member.user.username}が退出したよ`)
 		.setThumbnail(displayAvatarURL)
 	member.guild.channels.cache.get('992123731277258935').send({ 'embeds': [goodbyeEmbed] });
-	console.log(`${member.guild.name}から${member.user.username}が退出しました`);
-});
+	console.log(`${member.guild.name}から${member.user.username}が退出したよ`);
+})
 
-//VC通知(メッセージ送信がされない)
+//vcstate update
 client.on('voiceStateUpdate', (oldState, newState) => {
-    if (oldState.channelId === null && newState.channelId !== null) {
-			const connectEmbed = new EmbedBuilder()
-				.setColor(0x9999FF)
-				.setDescription(`${newState.channel.name}に${newState.member.user}が接続しました`)
-            .setThumbnail(newState.member.user.displayAvatarURL())
-        newState.guild.channels.cache.get('992123731277258935').send({ 'embeds': [connectEmbed] });
-		}
-    if (oldState.channelId !== null && newState.channelId === null) {
-			const disconnectEmbed = new EmbedBuilder()
-				.setColor(0x9999FF)
-				.setDescription(`${oldState.channel.name}に${oldState.member.user}が切断しました`)
-            .setThumbnail(oldState.member.user.displayAvatarURL())
-        oldState.guild.channels.cache.get('992123731277258935').send({ 'embeds': [disconnectEmbed] });
-		}
+	if (oldState.channelId === null && newState.channelId !== null) {
+		constconnectEmbed = newEmbedBuilder()
+			.setColor(0x99FFFF)
+			.setDescription(`${newState.channel.name}に${newState.member.user}が接続したよ`)
+			.setThumbnail(newState.member.user.displayAvatarURL())
+		newState.guild.channels.cache.get('992123731277258935')
+			.send({ 'embeds': [connectEmbed] });
+		console.log(`${newState.channel.name}に${newState.member.user}が接続したよ`);
+	}
+	if (oldState.channelId !== null && newState.channelId === null) {
+		constdisconnectEmbed = newEmbedBuilder()
+			.setColor(0x99FFFF)
+			.setDescription(`${oldState.channel.name}から${oldState.member.user}が切断したよ`)
+			.setThumbnail(oldState.member.user.displayAvatarURL())
+		oldState.guild.channels.cache.get('992123731277258935').send({ 'embeds': [disconnectEmbed] });
+		console.log(`${oldState.channel.name}から${oldState.member.user}が切断したよ`);
+	}
 })
